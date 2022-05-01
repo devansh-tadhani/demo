0de6f78e7d84e9f78158d56859d04f306356dbd0
Devansh Tadhani <62784304+devansh-tadhani@users.noreply.github.com>
2022-05-01 17:04:50 +0530

    Update main.tf
    Change name

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "first-tf-instance" {
   ami                    = "ami-04893cdb768d0f9ee"
   instance_type          = "t2.micro"
   tags = {
-    Name = "TFInst"
+    Name = "To change"
   }
 }
